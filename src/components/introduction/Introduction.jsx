ca4452480255f4c2350ee294d7685e27162b8fdd
NiamorDev <niamordev@proton.me>
2022-12-12 00:36:29 +0100

    update V3

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -6,19 +6,8 @@ export default function Home ({handleTest}) {
 
   return (
     <section id="introduction">
-      {/* <video className="" autoPlay loop muted id="videoIntro">
-        <source src="../../../assets/videos/hero.mp4" type="video/mp4" />
-        Your browser does not support HTML5 video.
-      </video> */}
 
-      {/* <img
-        id="bgMobile"
-        src="../../portfolio/assets/images/bg-small-screen-home2.png"
-        alt=""
-        className="small-screen hero"
-      /> */}
-
-      <img src="../../../logo.png" alt="" className="logo" />
+      <img src="../../../my-portfolio/logo.png" alt="" className="logo" />
 
       <div className="txt-home">
         <h1>Hello, I'm <span className="blue-text">Niamor.</span> </h1>
