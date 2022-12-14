fddfa53e02e5fa2a46a9b4e69b5b2f6a05bb48e7
NiamorDev <niamordev@proton.me>
2022-12-14 11:11:37 +0100

    update data

--- src/components/my-projects/MyProjects.jsx
+++ src/components/my-projects/MyProjects.jsx
@@ -6,12 +6,11 @@ import DataProjects from "../../data/DataProjects.json";
 export default function MyProjects({ opacity }) {
   const [dataProjects, setDataProjects] = useState([]);
 
-  const [filters, setFilters] = useState('')
-
+  const [filters, setFilters] = useState("");
 
   const handleFilterProjects = (e) => {
-    setFilters(e)
-  }
+    setFilters(e);
+  };
 
   useEffect(() => {
     setDataProjects(DataProjects);
@@ -20,57 +19,71 @@ export default function MyProjects({ opacity }) {
     <section id="myProjects">
       <h1 className="title">My project</h1>
       <div className="content">
-            <span className="intro">
-              <h2>Some of my training projects </h2>
-              <p>
-                (Thanks to{" "}
-                <a href="https://www.frontendmentor.io/" target="_blank">
-                  Frontend Mentor{" "}
-                </a>
-                website)
-              </p>
-            </span>
-            <div className="filter">
-              <button onClick={() => handleFilterProjects('')} className={filters==='' ?"btn-filter-selected" : null}>All</button>
-              <button onClick={() => handleFilterProjects('bigProject')} className={filters==='bigProject' ?"btn-filter-selected" : null}>Big projects</button>
-              <button onClick={() => handleFilterProjects('smallProject')} className={filters==='smallProject' ?"btn-filter-selected" : null}>Small projects</button>
-            </div>
+        <span className="intro">
+          <h2>Some of my training projects </h2>
+          <p>
+            (Thanks to{" "}
+            <a href="https://www.frontendmentor.io/" target="_blank">
+              Frontend Mentor{" "}
+            </a>
+            website)
+          </p>
+        </span>
+        <div className="filter">
+          <button
+            onClick={() => handleFilterProjects("")}
+            className={filters === "" ? "btn-filter-selected" : null}
+          >
+            All
+          </button>
+          <button
+            onClick={() => handleFilterProjects("bigProject")}
+            className={filters === "bigProject" ? "btn-filter-selected" : null}
+          >
+            Big projects
+          </button>
+          <button
+            onClick={() => handleFilterProjects("smallProject")}
+            className={
+              filters === "smallProject" ? "btn-filter-selected" : null
+            }
+          >
+            Small projects
+          </button>
+        </div>
 
-            <div className="all-projects">
-              {dataProjects.map((project) => {
-                if(project.level === filters ||filters === '')
-                return (
-                  <div>
-                    <a
-                      href={project.link}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      <div className="container-project">
-                        {project.importantProject ? (
-                          <span className="important-project">BEST</span>
-                        ) : null}
-                        <img src={project.img} alt="" />
-                        <div className="description">
-                          <div className="tags">
-                            {project.tags.map((tag) => (
-                              <p>{tag}</p>
-                            ))}
-                          </div>
+        <div className="all-projects">
+          {dataProjects.map((project) => {
+            if (project.level === filters || filters === "")
+              return (
+                <div>
+                  <a
+                    href={project.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <div className="container-project">
+                      <img src={project.img} alt="" />
+                      <div className="description">
+                        <div className="tags">
+                          {project.tags.map((tag) => (
+                            <p>{tag}</p>
+                          ))}
                         </div>
                       </div>
-                    </a>
+                    </div>
+                  </a>
 
-                    {project.codeSource !== null ? (
-                      <a href={project.codeSource} target="_blank">
-                        Source code
-                      </a>
-                    ) : null}
-                  </div>
-                );
-              })}
-            </div>
+                  {project.codeSource !== null ? (
+                    <a href={project.codeSource} target="_blank">
+                      Source code
+                    </a>
+                  ) : null}
+                </div>
+              );
+          })}
         </div>
+      </div>
     </section>
   );
 }
